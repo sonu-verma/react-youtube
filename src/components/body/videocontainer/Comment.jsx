a8dd02eb19ca77bddc4bbfdce26733a1f5d657e7
Sonu Verma <sonu.verma0715@gmail.com>
2025-04-19 16:55:38 +0530

    added nesting comments

--- src/components/body/videocontainer/Comment.jsx
+++ src/components/body/videocontainer/Comment.jsx
@@ -1,7 +1,9 @@
 const Comment = ({comment}) => {
     return <>
-        <div className="comment-container flex items-center gap-4 bg-gray-200 p-2 m-2">
-            <h2 className="bg-gray-100 rounded-full p-4"><img src={`https://ui-avatars.com/api/?name=${comment.author}`} alt="profileName" /></h2>
+        <div className="comment-container flex items-center gap-4 bg-gray-200 p-2 m-2 rounded-sm shadow-md">
+            <h2 className="bg-gray-100 rounded-full p-4">
+                <img className="w-[20px] h-[20px]" src={`https://ui-avatars.com/api/?name=${comment.author}`} alt="profileName" />
+                </h2>
             <div>
                 <h1>{comment?.author}</h1>
                 <p>{comment?.text}</p>
